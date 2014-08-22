67c723eef2e37375d8f016055e72d563ddb72cdd
pranoy <pranoy.iem@gmail.com>
2014-08-22 21:05:39 +0530

    Initial code base

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('hacksterApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
