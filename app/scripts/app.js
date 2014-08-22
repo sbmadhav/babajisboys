3f3fdada2221e2c29d0661fd89d3112c157cb8f8
pranoy <pranoy.iem@gmail.com>
2014-08-22 23:34:29 +0530

    abc

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,31 @@
 'use strict';
 
+/**
+ * @ngdoc overview
+ * @name ebusninessCardApp
+ * @description
+ * # ebusninessCardApp
+ *
+ * Main module of the application.
+ */
 angular
-  .module('hacksterApp', []);
+  .module('hacksterApp', [
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })      
+      .when('/theme', {
+        templateUrl: 'views/theme.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,27 @@
 'use strict';
 
 angular.module('hacksterApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+  .controller('MainCtrl', function ($scope, $location) {
+    $scope.fontSize = [
+      '14',
+      '16',
+      '18',
+      '14',
+      '16',
+      '18'
     ];
+    var options = [{
+       name: 'Something Cool',
+       value: 'something-cool-value'
+    }, {
+       name: 'Something Else',
+       value: 'something-else-value'
+    }];
+      
+    $scope.submitData = function() {
+        var formData = $("#form-data").serializeArray();
+        sessionStorage.setItem("customerData", formData);
+        $location.path("/theme");
+    }
+      
   });
